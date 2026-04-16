8172fd79866c02ea4888e047b58ec87751bfc07e
Sebastian Ramirez <sebastian.jose.ramirez.a@gmail.com>
2026-04-16 18:06:02 -0400

    feat: Intial implementation of spawn_service

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <pthread.h>
 #include "orchestrator.h"
+#include <sys/resource.h>
 
 /**
  * TODO: Implementar el despliegue del microservicio.
@@ -17,7 +18,7 @@
  * - Retornar el PID asignado.
  */
 int spawn_service(int index) {
-    pid_t pid;
+    pid_t pid = fork();
 
     // TODO: Invocar la creación del proceso hijo.
 
@@ -26,5 +27,35 @@ int spawn_service(int index) {
     // - Lógica del proceso HIJO (Setup de límites y Ejecución).
     // - Lógica del proceso PADRE (Gestión del dashboard).
 
-    return 0; // Cambiar por el PID real
+    // - Error en la creación del proceso.
+    if (pid < 0) {
+        dashboard[index].state = STATE_CRASHED;
+        return -1; // Error al crear el proceso
+    } 
+    // - Lógica del proceso HIJO (Setup de límites y Ejecución).
+    else if (pid == 0) {
+
+        struct rlimit limit;
+        limit.rlim_cur = dashboard[index].mem_limit; 
+        limit.rlim_max = dashboard[index].mem_limit;
+
+        if (setrlimit(RLIMIT_AS, &limit) == -1) {
+            perror("Error al establecer límite");
+            exit(EXIT_FAILURE);
+        }
+        
+        char *args[] = {dashboard[index].path, NULL}; 
+        execvp(args[0], args); 
+
+        // Si execv retorna, es que hubo un error
+        dashboard[index].state = STATE_CRASHED;
+        exit(EXIT_FAILURE);
+    } 
+    // - Lógica del proceso PADRE (Gestión del dashboard).
+    else {
+        dashboard[index].pid = pid;
+        dashboard[index].state = STATE_RUNNING;
+    }
+
+    return pid; // Cambiar por el PID real
 }
